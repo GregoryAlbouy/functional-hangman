05010b5f885b5d12423288f566cc3ac8c05b57a5
Gregory Albouy <gregoryalbouy75@gmail.com>
2022-06-19 16:18:04 +0200

    Refactor state tests
    - per function
    - with test cases

--- tests/EngineTests.elm
+++ tests/EngineTests.elm
@@ -5,44 +5,116 @@ import Expect
 import Test exposing (..)
 
 
-testEmptyState : Test
-testEmptyState =
-    describe "empty state"
-        [ test "isStarted" (\_ -> Expect.false "" (isStarted Engine.empty))
-        , test "isWon" (\_ -> Expect.false "" (isWon Engine.empty))
-        , test "isLost" (\_ -> Expect.false "" (isLost Engine.empty))
-        , test "isOver" (\_ -> Expect.false "" (isOver Engine.empty))
-        ]
+testIsStarted : Test
+testIsStarted =
+    let
+        cases =
+            [ { name = "empty is not started"
+              , model = Engine.empty
+              , exp = Expect.false
+              }
+            , { name = "init is started"
+              , model = initModel
+              , exp = Expect.true
+              }
+            , { name = "won is started"
+              , model = wonModel
+              , exp = Expect.true
+              }
+            , { name = "lost is started"
+              , model = lostModel
+              , exp = Expect.true
+              }
+            ]
+
+        run c =
+            test c.name (\_ -> c.exp "" (isStarted c.model))
+    in
+    describe "isStarted" (List.map run cases)
 
 
-testRunningState : Test
-testRunningState =
-    describe "running state"
-        [ test "isStarted" (\_ -> Expect.true "" (isStarted initModel))
-        , test "isWon" (\_ -> Expect.false "" (isWon initModel))
-        , test "isLost" (\_ -> Expect.false "" (isLost initModel))
-        , test "isOver" (\_ -> Expect.false "" (isOver initModel))
-        ]
+testIsWon : Test
+testIsWon =
+    let
+        cases =
+            [ { name = "empty is not won"
+              , model = Engine.empty
+              , exp = Expect.false
+              }
+            , { name = "init is not won"
+              , model = initModel
+              , exp = Expect.false
+              }
+            , { name = "won is won"
+              , model = wonModel
+              , exp = Expect.true
+              }
+            , { name = "lost is not won"
+              , model = lostModel
+              , exp = Expect.false
+              }
+            ]
+
+        run c =
+            test c.name (\_ -> c.exp "" (isWon c.model))
+    in
+    describe "isWon" (List.map run cases)
 
 
-testWonState : Test
-testWonState =
-    describe "won state"
-        [ test "isStarted" (\_ -> Expect.true "" (isStarted wonModel))
-        , test "isWon" (\_ -> Expect.true "" (isWon wonModel))
-        , test "isLost" (\_ -> Expect.false "" (isLost wonModel))
-        , test "isOver" (\_ -> Expect.true "" (isOver wonModel))
-        ]
+testIsLost : Test
+testIsLost =
+    let
+        cases =
+            [ { name = "empty is not lost"
+              , model = Engine.empty
+              , exp = Expect.false
+              }
+            , { name = "init is not lost"
+              , model = initModel
+              , exp = Expect.false
+              }
+            , { name = "won is not lost"
+              , model = wonModel
+              , exp = Expect.false
+              }
+            , { name = "lost is lost"
+              , model = lostModel
+              , exp = Expect.true
+              }
+            ]
+
+        run c =
+            test c.name (\_ -> c.exp "" (isLost c.model))
+    in
+    describe "isLost" (List.map run cases)
 
 
-testLostState : Test
-testLostState =
-    describe "lost state"
-        [ test "isStarted" (\_ -> Expect.true "" (isStarted lostModel))
-        , test "isWon" (\_ -> Expect.false "" (isWon lostModel))
-        , test "isLost" (\_ -> Expect.true "" (isLost lostModel))
-        , test "isOver" (\_ -> Expect.true "" (isOver lostModel))
-        ]
+testIsOver : Test
+testIsOver =
+    let
+        cases =
+            [ { name = "empty is not over"
+              , model = Engine.empty
+              , exp = Expect.false
+              }
+            , { name = "init is not over"
+              , model = initModel
+              , exp = Expect.false
+              }
+            , { name = "won is over"
+              , model = wonModel
+              , exp = Expect.true
+              }
+            , { name = "lost is over"
+              , model = lostModel
+              , exp = Expect.true
+              }
+            ]
+
+        run c =
+            test c.name (\_ -> c.exp "" (isOver c.model))
+    in
+    describe "isOver" (List.map run cases)
 
 
 testPickLetter : Test
